dc808d6f00b4ef1b77c440a50651807dc884e765
polonecmichal <polonecmichal36@gmail.com>
2025-10-04 19:20:53 +0200

    Add Uloha.py and Uloha2B.py

---
+++ Uloha.py
@@ -0,0 +1,1 @@
+# Naprogramuj program, ktorá vypíše čísla od 1 po N a ich druhé mocniny. Pričom N je argumentom programu
---
+++ Uloha2B.py
@@ -0,0 +1,6 @@
+n=int(input("Zadaj n: "))
+numbers=""
+for i in range(1,n+1):
+    numbers+=str(i)+" "
+
+print(numbers)
